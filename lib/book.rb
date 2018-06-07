767b5abeef7566b5cbf856e9bc8d1b6775199991
lilycampbell <lilycatcampbell@gmail.com>
2018-06-07 22:13:34 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,12 @@
 class Book
-  def initialize(color, number_of_pages, genre)
-    @color = color
+  def initialize(title, number_of_pages, genre)
+    @title = title
     @number_of_pages = number_of_pages
     @genre = genre
   end 
 end
 
- book_1 = Book.new("red", "204", "fiction")
+ book_1 = Book.new("Harry Potter", "204", "fiction")
  book_2 = Book.new("yellow", "378", "nonfiction")
  book_3 = Book.new("blue", "167", "history")
  
